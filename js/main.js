b71a0bab2068f8702fb2b9780bdc2ce17be2a852
Olga <o.nikolaichenko@ya.ru>
2023-12-18 16:29:42 +0100

    game works

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-let randomNumber = Math.floor(Math.random() * 100) + 1;
+const randomNumber = Math.floor(Math.random() * 100) + 1;
 
-let guesses = document.querySelector(".guesses");
-let lastResult = document.querySelector(".lastResult");
-let lowOrHi = document.querySelector(".lowOrHi");
+const guesses = document.querySelector(".guesses");
+const lastResult = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
 
-let guessSubmit = document.querySelector(".guessSubmit");
-let guessField = document.querySelector(".guessField");
+const guessSubmit = document.querySelector(".guessSubmit");
+const guessField = document.querySelector(".guessField");
 
-let guessCount = 1;
+const guessCount = 1;
 let resetButton;
 
 function checkGuess() {
@@ -38,4 +38,35 @@ function checkGuess() {
   guessCount++;
   guessField.value = "";
   guessField.focus();
+}
+
+guessSubmit.addEventListener("click", checkGuess);
+
+function setGameOver() {
+  guessField.disabled = true;
+  guessSubmit.disabled = true;
+  resetButton = document.createElement("button");
+  resetButton.textContent = "Start new game";
+  document.body.appendChild(resetButton);
+  resetButton.addEventListener("click", resetGame);
+}
+
+function resetGame() {
+  guessCount = 1;
+
+  const resetParas = document.querySelectorAll(".resultParas p");
+  for (let i = 0; i < resetParas.length; i++) {
+    resetParas[i].textContent = "";
+  }
+
+  resetButton.parentNode.removeChild(resetButton);
+
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+  guessField.value = "";
+  guessField.focus();
+
+  lastResult.style.backgroundColor = "white";
+
+  randomNumber = Math.floor(Math.random() * 100) + 1;
 }
