d0a71a8ed059dbe906cf6f64b3bb3f4656e69fb7
Olga <o.nikolaichenko@ya.ru>
2023-12-19 13:14:13 +0100

    errors are fixed

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 "use strict";
 
-const randomNumber = Math.floor(Math.random() * 100) + 1;
+let randomNumber = Math.floor(Math.random() * 100) + 1;
 
-const guesses = document.querySelector(".guesses");
-const lastResult = document.querySelector(".lastResult");
-const lowOrHi = document.querySelector(".lowOrHi");
+let guesses = document.querySelector(".guesses");
+let lastResult = document.querySelector(".lastResult");
+let lowOrHi = document.querySelector(".lowOrHi");
 
-const guessSubmit = document.querySelector(".guessSubmit");
-const guessField = document.querySelector(".guessField");
+let guessSubmit = document.querySelector(".guessSubmit");
+let guessField = document.querySelector(".guessField");
 
-const guessCount = 1;
+let guessCount = 1;
 let resetButton;
 
 guessField.focus();
